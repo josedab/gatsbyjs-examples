f551d7ac987c0e69736532655f6300438c41c640
Jose David Baena <josedab@gmail.com>
2018-04-12 11:40:23 -0700

    Workshop: Website scratch: Css components

--- workshop-kyle-mathews/website-scratch/src/pages/my-second-page.js
+++ workshop-kyle-mathews/website-scratch/src/pages/my-second-page.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Link from "gatsby-link";
+import "./my-second-page.css";
 
 export default () => {
   return (
-    <div>
+    <div className="my-second-page">
       <h1>
         Second page
       </h1>
