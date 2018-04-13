4b77cd4b3eacb4dcc6f2941690176f560984935e
Jose David Baena <josedab@gmail.com>
2018-04-13 11:18:16 -0700

    Workshop: Wordpress tutorial: Blog posts index

--- workshop-kyle-mathews/tutorial-wordpress/gatsby-config.js
+++ workshop-kyle-mathews/tutorial-wordpress/gatsby-config.js
@@ -2,5 +2,33 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    /*
+     * Gatsby's data processing layer begins with “source”
+     * plugins. Here the site sources its data from Wordpress.
+     */
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        /*
+         * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
+         * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
+         */
+        baseUrl: "dev-gatbsyjswp.pantheonsite.io",
+        // The protocol. This can be http or https.
+        protocol: "http",
+        // Indicates whether the site is hosted on wordpress.com.
+        // If false, then the asumption is made that the site is self hosted.
+        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+        // If your site is hosted on wordpress.org, then set this to false.
+        hostingWPCOM: false,
+        // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
+        // This feature is untested for sites hosted on Wordpress.com.
+        // Defaults to true.
+        useACF: true
+
+      },
+    },
+  ]
 }
--- workshop-kyle-mathews/tutorial-wordpress/src/pages/index.js
+++ workshop-kyle-mathews/tutorial-wordpress/src/pages/index.js
@@ -1,13 +1,58 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+const Post = (node) => {
+  return (
+    <div key={node.id}>
+      <Link
+        to={node.slug}
+        css={{ textDecoration: `none`, color: `inherit` }}
+      >
+        <h3>
+          {node.title}{" "}
+          <span>— {node.date}</span>
+        </h3>
+      </Link>
+      <p>
+        <div dangerouslySetInnerHTML={ {__html: node.content} }/>
+      </p>
+    </div>
+  )
+}
+
+const IndexPage = ({data}) => {
+
+  return (
+    <div>
+      <h1>These are the wordpress posts</h1>
+      <h4>
+        Posts:
+      </h4>
+      {data.allWordpressPost.edges.map(({ node }) =>
+        <Post {...node} />
+      )}
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </div>
+  )
+};
+
+export const query = graphql`
+  query IndexQuery {
+    allWordpressPost {
+      edges {
+        node {
+          id
+          slug
+          title
+          content
+          excerpt
+          date
+          modified
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
