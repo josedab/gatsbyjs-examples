95c3085557834ed5cbaee6415229dcd64af83f34
Jose David Baena <josedab@gmail.com>
2018-04-13 12:09:27 -0700

    Workshop: Wordpress example: Gatsby node api implementation for post creation

--- workshop-kyle-mathews/tutorial-wordpress/gatsby-node.js
+++ workshop-kyle-mathews/tutorial-wordpress/gatsby-node.js
@@ -3,5 +3,41 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require(`path`);
 
- // You can delete this file if you're not using it
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allWordpressPost {
+          edges {
+            node {
+              id
+              slug
+              title
+              content
+              excerpt
+              date
+              modified
+            }
+          }
+        }
+      }
+    `).then(result => {
+      console.log("RESULT", result);
+      result.data.allWordpressPost.edges.forEach(({ node }) => {
+        console.log("Creating page: " + node.slug);
+        createPage({
+          path: node.slug,
+          component: path.resolve(`./src/templates/blog-post.js`),
+          context: {
+            // Data passed to context is available in page queries as GraphQL variables.
+            slug: node.slug,
+          },
+        })
+      })
+      resolve()
+    })
+  })
+};
